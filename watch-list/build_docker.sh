c839a0433dd9ddd9078e396e57b08959c243d323
alexey_kluev <alexkluev@list.ru>
2025-10-17 01:12:38 +0300

    add custom docker build scripts

--- watch-list/build_docker.sh
+++ watch-list/build_docker.sh
@@ -1,1 +1,2 @@
-docker build -t io.kluev/watch-list:latest .
+version=$(grep "version =" build.gradle | awk -F"'" '{print $2}')
+docker build -t io.kluev/watch-list:latest -t io.kluev/watch-list:"$version" .
