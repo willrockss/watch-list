d191fc1526a1e5695c4eef193a10c7cdc7c8ded9
alexey_kluev <alexkluev@list.ru>
2025-10-27 22:59:16 +0300

    fix sh script

--- watch-list/build_docker.sh
+++ watch-list/build_docker.sh
@@ -1,2 +1,2 @@
-version=$(grep "version =" build.gradle | awk -F"'" '{print $2}')
+version=$(grep 'appVersion' gradle.properties | cut -d'=' -f2)
 docker build -t io.kluev/watch-list:latest -t io.kluev/watch-list:"$version" .
